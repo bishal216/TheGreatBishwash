6536292c1a1c7f5c0e40bb4c403e144ae2cf772a
Bellatrix <bis3hal@gmail.com>
2025-01-05 10:27:28 +0545

    Swaps Enter and Delete

--- utilities/virtual-keyboard/keyboard.js
+++ utilities/virtual-keyboard/keyboard.js
@@ -17,7 +17,7 @@ const toggleVirtualKeyboard = () => {
     const rows = [
       ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
       ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-      ["DEL", "Z", "X", "C", "V", "B", "N", "M", "ENTER"],
+      ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DEL"],
     ];
 
     rows.forEach((row) => {
